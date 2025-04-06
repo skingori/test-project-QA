21f5ccd09af67b53ea35d06059641d289fcd5b8a
skingori <infor.samson@gmail.com>
2025-04-06 13:09:30 +0300

    Remove comments on the db

--- e2e/db/initDatabase.ts
+++ e2e/db/initDatabase.ts
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Database, open } from "sqlite";
 import sqlite3 from "sqlite3";
 
